e45f33a813c122b5e7a81582e9ce879a481a4a4c
hopestephens <46386057+hopestephens@users.noreply.github.com>
2021-04-27 18:52:20 -0400

    added text box, buttons, and mouseover

--- testscript.js
+++ testscript.js
@@ -13,5 +13,35 @@ myitem.addEventListener("mouseout", mouseout);
 function mouseout() {
   myitem.style.color = "";
   myitem.style.fontWeight = "";
+}
 
+thebutton = document.getElementById("thebutton");
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {
+    otheritem.style.color = "red";
+}
+
+theotherbutton = document.getElementById("theotherbutton");
+otheritem2 = document.getElementById("buttontest2");
+theotherbutton.addEventListener("click", onButton2Click);
+function onButton2Click() {
+    otheritem2.style.color = "blue";
+}
+
+textentry = document.getElementById("myinput");
+textentry.addEventListener("change", onChange);
+function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
+}
+
+document.getElementById("mouseover").addEventListener("mouseover", mouseOver);
+document.getElementById("mouseover").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("mouseover").style.color = "red";
+}
+function mouseOut() {
+  document.getElementById("mouseover").style.color = "black";
 }
